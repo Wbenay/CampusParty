b9c5695da40903605ff6d729cce1dffee3d39c9b
Wbenay Soares <wbenay@softprime.com.br>
2019-09-04 10:53:19 -0300

    Inclusão do Projeto

---
+++ AppCampusPartyDemo/AppCampusPartyDemo/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppCampusPartyDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            buttonAtualizar.Clicked += ButtonAtualizar_Clicked;
+        }
+
+        private async void ButtonAtualizar_Clicked(object sender, EventArgs e)
+        {
+
+            indicatorRun.IsRunning = true;
+
+            var client = new System.Net.Http.HttpClient();
+
+            var response = await client.GetAsync("https://www.mercadobitcoin.net/api/BTC/day-summary/2019/9/02/");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                var result = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.Summary>(content);
+
+                labelPrecoAbertura.Text = string.Format("{0:C}", result.opening);
+                labelPrecoFechamento.Text = string.Format("{0:C}", result.closing);
+
+                indicatorRun.IsRunning = false;
+                return;
+            }
+            await DisplayAlert("Falha", "Não foi possível", "OK");
+        }
+    }
+}
---
+++ AppCampusPartyDemo/AppCampusPartyDemo/Models/Summary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCampusPartyDemo.Models
+{
+    public class Summary
+    {
+        public string date { get; set; }
+        public decimal opening { get; set; }
+        public decimal closing { get; set; }
+        public string lowest { get; set; }
+        public string highest { get; set; }
+        public string volume { get; set; }
+        public string quantity { get; set; }
+        public string amount { get; set; }
+        public string avg_price { get; set; }
+    }
+}
